8bc13e9032164bb96872d25a94051657af076d41
Adil Can <adil.can@Can-FVFY328UHV29.local>
2019-07-17 11:05:42 -0700

    group

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -6,14 +6,14 @@ s3 = session.resource('s3')
 
 @click.group()
 def cli():
-    "webrton deploy websites to AWS"
-    pass
+        "webrton deploy websites to AWS"
+        pass
 
-@click.command('list-buckets')
+@cli.command('list-buckets')
 def list_buckets():
-    "list all s3 buckets"
-    for bucket in s3.buckets.all():
-        print(bucket)
+        "list all s3 buckets"
+        for bucket in s3.buckets.all():
+                print(bucket)
 
 if __name__ == '__main__':
-    cli()
+        cli()

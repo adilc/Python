4235c2fe7fb043096b9fc8752b6e3153b1b5d218
Adil Can <adil.can@Can-FVFY328UHV29.local>
2019-07-17 12:12:17 -0700

    s3 static site

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -4,10 +4,12 @@ import click
 session = boto3.Session(profile_name='ninjaclirole')
 s3 = session.resource('s3')
 
+
 @click.group()
 def cli():
         "webrton deploy websites to AWS"
         pass
+
 
 @cli.command('list-buckets')
 def list_buckets():
@@ -15,12 +17,53 @@ def list_buckets():
         for bucket in s3.buckets.all():
                 print(bucket)
 
+
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
         "list bucket objects"
         for obj in s3.Bucket(bucket).objects.all():
                 print(obj)
+
+
+@cli.command('staticwebsite')
+@click.argument('bucket')
+def staticwebsite(bucket):
+        "Create and Configure S3 Bucket"
+        s3_bucket = s3.create_bucket(
+                Bucket=bucket,
+                CreateBucketConfiguration={'LocationConstraint': session.region_name}
+        )
+        policy = """
+        {
+                "Version":"2012-10-17",
+                "Statement":[{
+                "Sid":"PublicReadGetObject",
+                "Effect":"Allow",
+                "Principal": "*",
+                "Action":["s3:GetObject"],
+                "Resource":["arn:aws:s3:::%s/*"
+                ]
+                }
+                ]
+        }
+        """ % s3_bucket.name
+        policy = policy.strip()
+
+        pol = s3_bucket.Policy()
+        pol.put(Policy=policy)
+        ws = s3_bucket.Website()
+        ws.put(WebsiteConfiguration={
+                'ErrorDocument': {
+                        'Key': 'error.html'
+                },
+                'IndexDocument': {
+                        'Suffix': 'index.html'
+                }
+        })
+        return
+        
+
 
 if __name__ == '__main__':
         cli()
